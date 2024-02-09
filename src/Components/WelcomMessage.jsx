9ddcf6ddfbb33108943d667886342086e3a4abfa
SohailSutar <sohailsutar6221@gmail.com>
2024-02-09 16:12:36 +0530

    Initial files add

---
+++ src/Components/WelcomMessage.jsx
@@ -0,0 +1,7 @@
+import styles from "./WelcomeMessage.module.css";
+
+const WelcomeMessage = () => {
+  return <p className={styles.welcome}>`No Todo's Present....`<br/><span></span></p>;
+};
+
+export default WelcomeMessage;
